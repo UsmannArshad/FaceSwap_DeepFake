4159f86f24f44a6d69766b8a1b68d1e5755f35a7
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-11 16:06:47 +0500

    Convex Hull and Landmarks

--- FaceSwap.py
+++ FaceSwap.py
@@ -4,6 +4,26 @@ import cmake
 import dlib
 
 frontal_face_detector=dlib.get_frontal_face_detector()
-frontal_shape_detector=dlib.shape_predictor("dataset/shape_predictor_68_face_landmarks")
-imu=cv.imread("./image/imu.jpg",0)
-cv.imshow("Imran Khan",imu)
+frontal_shape_predictor=dlib.shape_predictor("./dataset/shape_predictor_68_face_landmarks.dat")
+imu_bgr=cv.imread("./image/imu.jpg")
+imu=cv.cvtColor(imu_bgr,cv.COLOR_BGR2GRAY)
+#cv.imshow("Imran Khan",imu)
+modi=cv.imread("./image/modi.jpg")
+#cv.imshow("Narendra Modi",modi)
+source_image_canvas=np.zeros_like(imu)
+height,width,no_of_channels=modi.shape
+destination_image_canvas=np.zeros((height,width,no_of_channels),np.uint8)
+source_faces=frontal_face_detector(imu)
+for source_face in source_faces:
+    source_face_landmarks=frontal_shape_predictor(imu,source_face)
+    source_face_landmark_points=[]
+    for landmark_no in range(0,68):
+        x_point=source_face_landmarks.part(landmark_no).x
+        y_point=source_face_landmarks.part(landmark_no).y
+        source_face_landmark_points.append((x_point,y_point))
+        cv.circle(imu_bgr,(x_point,y_point),2,(255,0,0),-1)
+        cv.imshow("landmarked",imu_bgr)
+    source_face_landmark_points_array=np.array(source_face_landmark_points,np.int32)
+    source_face_convexhull=cv.convexHull(source_face_landmark_points_array)
+    cv.polylines(imu_bgr, [source_face_convexhull], True, (255,0,0),3)
+    cv.imshow("Convex Hull",imu_bgr)
